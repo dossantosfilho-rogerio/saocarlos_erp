fe761852cab4d5f3294bfc4a94d58051798fe911
Rogério Guimarães <dossantosfilhorogerio525@gmail.com>
2026-05-04 16:58:20 -0300

    landing page

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return redirect()->route('login');
+    return view('welcome');
 });
 
 Route::get('/login', Login::class)->name('login')->middleware('guest');
